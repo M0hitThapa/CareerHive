154678052798124ba85cf279a7e3c2170f6f30c4
Mohit Thapa <thapamohit2058@gmail.com>
2025-01-05 23:00:02 +0545

    completed the backend part of my job portal application

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -72,10 +72,11 @@ export const getJobs = asyncHandler(async(req,res) => {
   try {
    const jobs = await Job.find({}).populate(
     "createdBy",
-    "name email profilePicture"
-   );
+    "name profilePicture"
+   )
+   .sort({ createdAt: -1 });
 
-    console.log("Jobs: ", jobs);
+    return res.status(200).json(jobs);
   } catch (error) {
     console.log("Error in getJobs: ", error);
     return res.status(500).json({
@@ -83,3 +84,168 @@ export const getJobs = asyncHandler(async(req,res) => {
     })
   }
 })
+
+
+export const getJobsByUser = asyncHandler(async (req,res) => {
+  try {
+    const user = await User.findById( req.params.id );
+    if(!user) {
+      res.status(404).json({ message: "User not found" })
+    }
+
+    const jobs = await Job.find({createdBy: user._id}).populate("createdBy",
+      "name profilePicture"
+    );
+    return res.status(200).json(jobs);
+  } catch (error) {
+  console.log ("Error in getJobsByUser: ", error);
+  return res.status(500).json({
+    message: "Server Error",
+  })
+ 
+  }
+})
+
+
+export const searchJobs = asyncHandler(async (req,res) => {
+  try {
+    const {tags,location,title} = req.query;
+
+    let query = {};
+
+    if (tags) {
+      query.tags = { $in: tags.split(",") };
+    }
+
+    if (location) {
+      query.location = { $regex: location, $option: "i" };
+    }
+
+    if (title) {
+      query.title = { $regex:title, $options: "i" };
+    }
+
+    const jobs  = await Job.find(query).populate("createdBy", "name proPicture");
+
+    return res.status(200).json(jobs);
+  
+  } catch (error) {
+    console.log("Error in searchJobs: ", error);
+    return res.status(500).json({
+      message: "Server Error",
+    })
+  }
+})
+
+export const applyJob = asyncHandler(async (req, res)  => {
+try {
+  const job = await Job.findById(req.params.id);
+
+  if (!job) {
+    return res.status(404).json({message: "Job not found" });
+  }
+
+  const user = await User.findOne({ auth0Id: req.oidc.user.sub });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not Found "})
+  }
+
+  if (job.applicants.includes(user._id)) {
+    return res.status(400).json({ message: "Already applied for this job"})
+  }
+
+  job.applicants.push(user._id);
+
+  await job.save();
+
+  return res.status(200).json(job);
+} catch (error) {
+  console.log("Error in applyJob: ", error);
+  return res.status(500).json({
+    message: "Server Error",
+  })
+}
+})
+export const likeJob = asyncHandler(async (req, res) => {
+  try {
+    const job = await Job.findById(req.params.id);
+
+    if (!job) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+
+    const user = await User.findOne({ auth0Id: req.oidc.user.sub });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isLiked = job.likes.includes(user._id);
+
+    if (isLiked) {
+      job.likes = job.likes.filter((like) => !like.equals(user._id));
+    } else {
+      job.likes.push(user._id);
+    }
+
+    await job.save();
+
+    return res.status(200).json(job);
+  } catch (error) {
+    console.log("Error in likeJob: ", error);
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
+});
+
+export const getJobById = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const job = await Job.findById(id).populate(
+      "createdBy",
+      "name profilePicture"
+    );
+
+    if (!job) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+
+    return res.status(200).json(job);
+  } catch (error) {
+    console.log("Error in getJobById: ", error);
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
+});
+
+export const deleteJob = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const job = await Job.findById(id);
+    const user = await User.findOne({ auth0Id: req.oidc.user.sub });
+
+    if (!job) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    await job.deleteOne({
+      _id: id,
+    });
+
+    return res.status(200).json({ message: "Job deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleteJob: ", error);
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
+});
